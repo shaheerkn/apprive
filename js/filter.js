ce0c9130f5d762aa7aeddc07d6fa04a3321e5a35
abdullahProfile <abdullahdew88@gmail.com>
2026-01-19 13:20:36 +0500

    overview all pages and sections with figma, and also update classes names for some sections

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const openBtn = document.querySelector('.filter-btn');
-  const closeBtn = document.querySelector('.portfolio__filter-modal__close');
-  const modal = document.querySelector('.portfolio__filter-modal');
+  const closeBtn = document.querySelector('.filters-panel__filter-modal__close');
+  const modal = document.querySelector('.filters-panel__filter-modal');
 
   if (!openBtn || !closeBtn || !modal) return;
 
