54a2fe96edd9f2a15ffaa2f1e61324e86d9d927b
abdullahProfile <abdullahdew88@gmail.com>
2026-01-20 04:35:17 +0500

    review all pages

--- js/chalet-featured.js
+++ js/chalet-featured.js
@@ -11,11 +11,28 @@ document.addEventListener('DOMContentLoaded', function () {
       prevEl: '.swiper-button-prev',
     },
     breakpoints: {
+
+      400: {
+        slidesPerView: 1.1,
+        slidesPerGroup: 1,
+        spaceBetween: 20,
+      },
       768: {
         slidesPerView: 1.2,
         slidesPerGroup: 1,
         spaceBetween: 20,
       },
+
+      992: {
+        slidesPerView: 1.3,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        spaceBetween: 40,
+      }
     },
   });
 });
