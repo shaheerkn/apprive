31a191f2cfd2986113b46d729870666ef7949357
Muhammad Shaheer <shaheer.devv@gmail.com>
2026-03-03 04:07:16 +0500

    fixes

--- taxonomy-destination.php
+++ taxonomy-destination.php
@@ -217,8 +217,11 @@ $services_title = get_field('services_title', $destination) ?: 'Privately Orches
       </button>
     </div>
 
-    <a href="<?php echo home_url(); ?>/properties" class="btn btn--solid--primary">
-      <span class="">View All chalets in <?php echo esc_html($destination->name); ?></span>
+    <a href="<?php echo home_url(); ?>/properties" class="btn btn--solid--primary for-summer">
+      <span class="">View All villas in Mykonos</span>
+    </a>
+    <a href="<?php echo home_url(); ?>/properties" class="btn btn--solid--primary for-winter">
+      <span class="">View All chalets in Courchevel</span>
     </a>
   </div>
 </section>
